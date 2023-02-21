57788684d0c98bd13a24c7c04eab2e1ff937aa9e
rifatul-22 <rifatulislam455@gmail.com>
2023-02-22 03:01:30 +0600

    v 1.0.3

--- js/script.js
+++ js/script.js
@@ -4,5 +4,14 @@ $(function(){
         arrows: false,
         prevArrow:$('.inspiration_slick_icon_btn.prev'),
         nextArrow:$('.inspiration_slick_icon_btn.next')
-      });
+    });
+
+    // $('.toggle-button').onClick({
+       
+    // })
+    $('.celebration_slider').slick({
+        arrows: true,
+        prevArrow:$('.celebration_slider_icon_btn.prev'),
+        nextArrow:$('.celebration_slider_icon_btn.next')
+    });
 });
